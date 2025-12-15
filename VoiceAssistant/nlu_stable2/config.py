1c2026af3f71ada6e608862b92410d34a9e72d56
Shakhyar Gogoi <68093713+shakhyar@users.noreply.github.com>
2025-12-15 12:03:22 +0530

    Update config.py

--- VoiceAssistant/nlu_stable2/config.py
+++ VoiceAssistant/nlu_stable2/config.py
@@ -1,8 +1,9 @@
 hostname = ""
 ip_location_api_token = ""
-open_weather_api_token = "f317f1f507f2d9f0a8aa1316d86507b8"
+open_weather_api_token = ""
 
 dataset_path = 'VoiceAssistant/nlu_stable2/data/data.json'
 saved_params = 'saved/saved_params'
 saved_model = 'saved/model.pkl'
+
 saved_stemmer = 'saved/stemmer'
